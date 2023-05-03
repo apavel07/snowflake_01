4949fb4849c166b8976b41fab09827ff56e6d201
apavel07 <apavel07@gmail.com>
2023-05-03 18:59:34 +0200

    Hello

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import pandas
 import streamlit
 
+# https://apavel07-snowflake-01-streamlit-app-edh0as.streamlit.app/
+
 streamlit.title("Menu")
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -8,4 +10,5 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.dataframe(my_fruit_list)
