3b0258d9c49af1c0cd2217146f4cc7f3537d8448
apavel07 <apavel07@gmail.com>
2023-05-07 17:34:38 +0200

    Hello

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,25 @@
 import pandas
 import streamlit
+import requests
 
 # https://apavel07-snowflake-01-streamlit-app-edh0as.streamlit.app/
 
-streamlit.title("Menu")
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+# streamlit.title("Menu")
+# streamlit.header('Breakfast Menu')
+# streamlit.text('Omega 3 & Blueberry Oatmeal')
+# streamlit.text('Kale, Spinach & Rocket Smoothie')
+# streamlit.text('Hard-Boiled Free-Range Egg')
+#
+# my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# my_fruit_list = my_fruit_list.set_index('Fruit')
+# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# fruits_to_show = my_fruit_list.loc[fruits_selected]
+# if fruits_selected:
+#     streamlit.dataframe(fruits_to_show)
+# else:
+#     streamlit.dataframe(my_fruit_list)
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-if fruits_selected:
-    streamlit.dataframe(fruits_to_show)
-else:
-    streamlit.dataframe(my_fruit_list)
+
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
